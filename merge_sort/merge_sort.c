a1aeeb31a68f4b43966b6dc24e8df8d0568c0db7
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2020-08-26 22:12:48 +0200

    Initial version of c merge sort implementation

--- merge_sort/merge_sort.c
+++ merge_sort/merge_sort.c
@@ -1,18 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 
 
-int main(void)
+void printArray(int array[], int size) {
+	for ( int i = 0; i < size; i++) {
+		printf("%d, ", array[i]);
+	};
+	printf("\n");
+}
 
+
+int mergeLists(int list_to_sort[], int start_a, int start_b, int end_b) {
+	// copy list a to different list
+	// copy list b to different list
+	// iterate over both of them 
+	// replace elements in list_to_sort
+}
+
+
+void mergeSort(int list_to_sort[], int start_ix, int end_ix) {
+	if (start_ix < end_ix) {
+		int split_ix = (end_ix - start_ix) / 2;
+		printf("split_ix %d: \n", split_ix);
+		mergeSort(list_to_sort, start_ix, split_ix);
+		mergeSort(list_to_sort, split_ix + 1, end_ix);
+		mergeLists(list_to_sort, start_ix, split_ix + 1, end_ix);
+	}
+
+}
+
+
+int main()
 {
 	int list_to_sort[] = {9, 6, 5, 3, 1};
 	int size = sizeof(list_to_sort)/sizeof(list_to_sort[0]);
-	int i;
+	printf("\n %d \n", size);
+	printArray(list_to_sort, size);
+	mergeSort(list_to_sort, 0, size - 1);
+	printArray(list_to_sort, size);
 
-	splitter
-	for ( i = 0; i < size; i++ ) {
-		printf("indeks %d - wartość %d \n", i, list_to_sort[i]);
-	}
 	return 0;
 }
-
-i

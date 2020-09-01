b5276d2d64d9120559cb5caa4204ebc46c9731b9
Antoni Szczepanik <szczepanik.antoni@gmail.com>
2020-09-01 19:43:00 +0200

    up

--- exam/budki.py
+++ exam/budki.py
@@ -5,6 +5,7 @@
 
 from collections import namedtuple
 from random import choices
+from pprint import pprint
 
 Node = namedtuple('Node', ('n', 'm', 'r'))
 
@@ -13,9 +14,12 @@ def get_number_of_connections(nodes):
     # informacja o "najstarszym przodku" każdej budki (początkowo każda
     # wskazuje na siebie)
     roots = list(range(len(nodes)))
+    adjacency_list = {}
+    for node in nodes:
+        adjacency_list[node] = []
 
     # posortuj budki względem pierwszej współrzędnej
-    nodes_sorted = sorted(nodes) # n log(n)
+    nodes_sorted = sorted(nodes)
 
     for i, current_node in enumerate(nodes_sorted):
         # możemy spokojnie patrzeć tylko na 15 kolejnych budek - jeżeli na
@@ -23,10 +27,8 @@ def get_number_of_connections(nodes):
         # więcej (maksymalny promień to 50 -> maksymalna liczba sąsiadów = 15)
         if i + 16 >= len(nodes_sorted):
             next_nodes = nodes_sorted[i+1:]
-            assert len(next_nodes) <= 15
         else:
             next_nodes = nodes_sorted[i+1:i+16]
-            assert len(next_nodes) == 15
 
         for j, next_node in enumerate(next_nodes):
             # co najwyżej 15 O(n), więc nadal liniowo
@@ -36,15 +38,32 @@ def get_number_of_connections(nodes):
                 if calculate_distance(current_node, next_node) < min(next_node.r, current_node.r):
                     # zapisujemy wiadomość o "najstarszym przodku"
                     roots[next_node_ix] = roots[i]
+                    # dodajemy sąsiadującą budkę
+                    adjacency_list[current_node].append(next_node)
 
-    # zlicz unikalnych "najstarszych przodków"
-    unique = set()
-    for root in roots:
-        if root not in unique:
-            unique.add(root)
+    return get_number_of_connected_components(adjacency_list) -1
 
-    return len(unique) - 1
+def get_number_of_connected_components(adjacency_list):
+    # bfs zliczający liczbę połączonych komponentów
+    queue = []
+    # sprawdzaj czy dany node był już odwiedzony
+    visited = {}
+    for node in adjacency_list:
+        visited[node] = False
 
+    number_of_components = 0
+    for node in adjacency_list:
+        if not visited[node]:
+            queue.append(node)
+            number_of_components += 1
+            while queue:
+                current_node = queue.pop(0)
+                visited[current_node] = True
+                for adjacent_node in adjacency_list[current_node]:
+                    if not visited[adjacent_node]:
+                        queue.append(adjacent_node)
+
+    return number_of_components
 
 def calculate_distance(node_1, node_2):
     return ((node_1.n - node_2.n)**2 + (node_1.m - node_2.m)**2) ** (1/2)
@@ -52,10 +71,11 @@ def calculate_distance(node_1, node_2):
 
 if __name__ == '__main__':
     # dane testowe
-    l = 100000
-    ns = choices(range(1, 1000), k=l)
-    ms = choices(range(1, 1000), k=l)
-    rs = [50 for x in range(l)]
+    l = 10
+    ns = choices(range(1, 100), k=l)
+    ms = choices(range(1, 100), k=l)
+    rs = choices([50 for x in range(l)], k=l)
     nodes = [Node(n, m, r) for n, m, r, in zip(ns, ms, rs)]
+    print(nodes)
 
     print(get_number_of_connections(nodes))
